69eeb4927ad8019ea616d29ba0c6c0f260800f7b
danieleverrienti86 <danieleverrienti1986@gmail.com>
2025-03-21 08:14:48 +0100

    First commit

---
+++ springdoc-demo/src/test/java/com/redcatdev86/springdocdemo/SpringdocDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redcatdev86.springdocdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdocDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
